caea86efe94216961ac099897acb0e0e5b4c36dc
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-06-22 01:46:22 -0300

    adjustment setup

--- setup.py
+++ setup.py
@@ -3,13 +3,10 @@ from setuptools import setup
 setup(
     name='gitman',
     version='0.1.0',
-    py_modules=['gitman'],
-    install_requires=[
-        'typer',
-    ],
+    py_modules=['src.main'],
     entry_points={
         'console_scripts': [
-            'gitman=src.main:app',
+            'gitman = src.main:app',
         ],
     },
 )
