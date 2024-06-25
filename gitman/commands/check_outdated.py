341648d56d85261ff87805c7d9597b0272208c42
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-06-25 17:04:14 -0300

    check in folder has package

--- gitman/commands/check_outdated.py
+++ gitman/commands/check_outdated.py
@@ -7,17 +7,19 @@ def check_outdated(base_dir):
     for dir in os.listdir(base_dir):
         full_path = os.path.join(base_dir, dir)
         if os.path.isdir(full_path):
-            print(i18n.t('check_outdated.entering_directory', fullpath=full_path))
-            os.chdir(full_path)
-            
-            try:
-                print(i18n.t('check_outdated.running_outdated', fullpath=full_path))
-                subprocess.run(['npm', 'outdated'], check=True)
-            
-            except subprocess.CalledProcessError as e:
-                print(i18n.t('check_outdated.error', fullpath=full_path))
-                print(e.stderr)
-            
-            os.chdir('..')
+            package_json_path = os.path.join(full_path, 'package.json')
+            if os.path.exists(package_json_path):
+                print(i18n.t('check_outdated.entering_directory', fullpath=full_path))
+                os.chdir(full_path)
+                
+                try:
+                    print(i18n.t('check_outdated.running_outdated', fullpath=full_path))
+                    subprocess.run(['npm', 'outdated'], check=True)
+                
+                except subprocess.CalledProcessError as e:
+                    print(i18n.t('check_outdated.error', fullpath=full_path))
+                    print(e.stderr)
+                
+                os.chdir('..')
     
     print(i18n.t('check_status.complete_status'))
