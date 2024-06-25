1d73d696cb23256cc0d5e64164423731d9e3ef37
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-06-25 17:07:42 -0300

    check git folder

--- gitman/commands/check_status.py
+++ gitman/commands/check_status.py
@@ -7,17 +7,19 @@ def check_status(base_dir):
     for dir in os.listdir(base_dir):
         full_path = os.path.join(base_dir, dir)
         if os.path.isdir(full_path):
-            print(i18n.t('check_status.entering_directory', fullpath=full_path))
-            os.chdir(full_path)
-            
-            try:
-                print(i18n.t('check_status.checking_git_status', fullpath=full_path))
-                subprocess.run(['git', 'status'], check=True)
-            
-            except subprocess.CalledProcessError as e:
-                print(i18n.t('check_status.git_error', fullpath=full_path))
-                print(e.stderr)
-            
-            os.chdir('..')
+            git_folder = os.path.join(full_path, '.git')
+            if os.path.exists(git_folder):
+                print(i18n.t('check_status.entering_directory', fullpath=full_path))
+                os.chdir(full_path)
+                
+                try:
+                    print(i18n.t('check_status.checking_git_status', fullpath=full_path))
+                    subprocess.run(['git', 'status'], check=True)
+                
+                except subprocess.CalledProcessError as e:
+                    print(i18n.t('check_status.git_error', fullpath=full_path))
+                    print(e.stderr)
+                
+                os.chdir('..')
 
     print(i18n.t('check_status.complete_status'))
