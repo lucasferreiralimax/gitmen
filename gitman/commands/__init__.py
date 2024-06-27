b5c62f6562b8f86d4b0f38a3fd0d0be4c77a206e
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-06-27 03:23:06 -0300

    import new projects_update_from_check

--- gitman/commands/__init__.py
+++ gitman/commands/__init__.py
@@ -1,4 +1,4 @@
-from .projects_update import projects_update
+from .projects_update import projects_update, projects_update_from_check
 from .ncu_update import ncu_update
 from .get_cli_version import get_cli_version
 from .check_outdated import check_outdated
@@ -6,4 +6,4 @@ from .check_status import check_status
 from .check_github import check_github
 
 # Exportando funções/módulos
-__all__ = ["projects_update", "ncu_update", "get_cli_version", "check_outdated", "check_status", "check_github"]
+__all__ = ["projects_update", "ncu_update", "get_cli_version", "check_outdated", "check_status", "check_github", "projects_update_from_check"]
